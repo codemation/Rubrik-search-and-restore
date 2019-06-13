2e32719f3161a3b88369a16efcd7044116bc836e
josh <jjamison@joshs-MacBook-Pro.local>
2019-06-13 23:32:35 +0200

    Added restore.py & search.py

---
+++ restore.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+
+"""
+Created for assisting with CLI Restoration
+restore.py is dependent on admin-credentials file with username/password of user with appropiate permissions to CDM.
+see readme.md
+"""
+
+import os
+import datetime, time
+
+try:
+    with open('auth.cfg', 'r') as auth:
+        auth_path = auth.readline().rstrip()
+except:
+    auth_path = None
+    pass
+auth = '-H "Authorization: Basic $(cat %s | base64)"'%(auth_path) if auth_path is not None else None
+
+headers = " --header 'Content-Type: application/json' --header 'Accept: application/json'"
+
+def jsonify_path(toConvert):
+    """
+        converts a single or comma separated set of path inputs into a stringified list ["path","path","path"] to be used by curl
+    """
+    files = '['
+    if toConvert.split(',')[0] == toConvert:
+        path_split = toConvert
+        files = files + '"' + path_split + '"'
+    else:
+        path_split = toConvert.split(',')
+        for f in path_split:
+            files = files + '"' + f + '"'
+            if not path_split.index(f) == len(path_split) -1:
+                files = files + ','
+    files = files + ']'
+    print("Files: %s"%(files))
+    return files
+
+def get_curl_response(curl):
+    import os, json
+    os.system('%s | python -m json.tool > response.json'%(curl))
+    with open('response.json', 'r') as f:
+        response = json.load(f)
+        return response
+    
+def fileset(object_name, snapshotId, paths):
+    """
+
+    """
+    files = jsonify_path(paths)
+    pathString = "--data-binary '{"+ '"sourceDirs": %s }'%(files) + "'"
+    url = " 'https://%s/api/internal/fileset/snapshot/%s/download_files'"%(host, snapshotId)
+    header = "-H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*'"
+    curl = "curl -s -X POST %s %s %s %s --insecure"%(auth, header, pathString, url)
+    print(curl)
+    before_curl_time = datetime.datetime.utcnow()
+    response = get_curl_response(curl)
+    if not 'id' in response:
+        try:
+            print(response['message'])
+        except:
+            print(response)
+        return
+    jobInstanceId = response['id']
+    get_download(object_name, jobInstanceId, before_curl_time)
+
+def vm(object_name, snapshotId, paths):
+ 
+    files = jsonify_path(paths)
+    url = " 'https://%s/api/internal/vmware/vm/snapshot/%s/download_files'"%(host, snapshotId)
+    curl = "curl -s -X POST %s %s -d '{%s:%s}'"%(auth, headers,'"paths"', files) + url + ' --insecure'
+    print(curl)
+    before_curl_time = datetime.datetime.utcnow()
+    response = get_curl_response(curl)
+    if not 'id' in response:
+        try:
+            print(response['message'])
+        except:
+            print(response)
+        return
+    jobInstanceId = response['id']
+
+    get_download(object_name, jobInstanceId, before_curl_time)
+
+def get_download(object_name, jobInstanceId, before_curl_time):
+    after_date = '%s-%s-%sT%s%%3A%s'%(before_curl_time.year, before_curl_time.month, before_curl_time.day, before_curl_time.hour, before_curl_time.minute)
+    url1 = "'https://%s/api/internal/event_series?event_type=Recovery&object_name=%s&after_date=%s'"%(host, object_name,after_date)
+    curl1 = "curl -s -X GET %s %s "%(auth,headers) + url1 + ' --insecure'
+    print (curl1)
+
+    events = get_curl_response(curl1)
+    eventSeriesId = None
+    for event in events["data"]:
+        if jobInstanceId == event["jobInstanceId"]:
+            eventSeriesId = event['eventSeriesId']
+            url1_status = "'https://%s/api/internal/event_series/status'"%(host)
+            status_cfg = '[{"id": "%s", "jobInstanceId": "%s"}]'%(eventSeriesId, jobInstanceId) 
+            curl1_status = "curl -s -X POST %s %s -d '%s' "%(auth, headers, status_cfg) + url1_status + ' --insecure'
+            print(curl1_status)
+            while True:
+                ev_status = get_curl_response(curl1_status)
+                if not ev_status["data"][0]["progress"] == "100.00":
+                    time.sleep(5)
+                    print("Waiting for Download Link to be generated %s %% completed"%(ev_status["data"][0]["progress"]))
+                else:
+                    print("Successfully Completed Generation of FileDownload\n\n")
+                    break
+    # Get generated download link # 
+    url2 = "'https://%s/api/internal/event_series/%s?limit=2147483647'"%(host, eventSeriesId)
+    curl2 = "curl -s -X GET %s %s "%(auth, headers) + url2 + ' --insecure'
+    curl2_events = get_curl_response(curl2)['eventDetailList']
+
+    success = None
+    for ev in curl2_events: 
+        if ev["status"] == "Success":
+            success = ev
+            break
+        if ev["status"] == "Failure":
+            print("Restore aborted with error: \n  %s"%(ev["eventInfo"]))
+            return
+    import json
+    # Writes Job result to downloadpath.json to re-open
+    with open('downloadpath.json','w') as ev_dl_json:
+        ev_dl_json.write(success['eventInfo'])
+    # reloads result as json
+    with open('downloadpath.json', 'r') as f:
+        ev_dl = json.load(f)
+    
+    restoreName = ev_dl['downloadInfo']['name']
+    restorePath = ev_dl['downloadInfo']['path']
+    url_dload = "'https://%s/%s'"%(host, restorePath)
+    curl_dload = "curl -s %s %s -o %s --insecure"%(auth, url_dload, restoreName)
+    print ("Use the following command to download the specified files: \n %s"%(curl_dload))
+    return curl_dload
+def usage():
+    print("""
+    Usage:
+        restore.py <cdm_ip> <vm|fileset> <object_name> <snapshot_id> <filepaths>
+    Example:
+        restore.py 192.168.10.100 vm prod-ubuntu 2f386a65-ce27-4a46-ba8d-1c0d3a72281d /home/rksupport/data/10.txt,/home/rksupport/data/100.txt,/home/rksupport/test
+        restore.py 10.35.36.165 fileset data 0b62f222-c332-4920-a2ce-b885c913b49d /home/rksupport/data/67.txt,/home/rksupport/data/64.txt
+    """)
+
+def main():
+    if auth is None:
+        print("""
+        Missing auth.cfg file
+        Run:
+            echo -n "<path-to-auth-file>" > auth.cfg
+        Also required:
+            echo -n "admin:password" > path-to-auth-file # Keep this file in safe place
+        
+        Example:
+            echo -n "~/cdm_auth" > auth.cfg
+            echo -n "admin:abcd1234" > ~/cdm_auth
+        """)
+        return
+    import sys
+    args = sys.argv
+    global host
+    try:
+        host = args[1]
+        print("Host is %s"%(host))
+        objType, objName, snapId, filepaths = args[2], args[3], args[4], args[5]
+    except:
+        usage()
+        return
+    if objType == 'vm':
+        vm(objName, snapId, filepaths)
+    elif objType == 'fileset':
+        fileset(objName, snapId, filepaths)
+    else:
+        usage()
+if __name__ == "__main__":
+    main()
---
+++ search.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python3
+
+"""
+See README.MD
+"""
+
+from urllib.parse import quote
+
+try:
+    with open('auth.cfg', 'r') as auth:
+        auth_path = auth.readline().rstrip()
+except:
+    auth_path = None
+    pass
+auth = '-H "Authorization: Basic $(cat %s | base64)"'%(auth_path) if auth_path is not None else None
+
+def get_curl_response(curl):
+    import os, json
+    os.system('%s | python -m json.tool > response.json'%(curl))
+    with open('response.json', 'r') as f:
+        response = json.load(f)
+        return response
+
+def list_files(response, kw):
+    print("Files found with search path:")
+    for file in response:
+        print(file['path'])
+        if not 'files' in kw:
+            print("versions:")
+            oldest_v, oldest_s = file['fileVersions'][0]['lastModified'], file['fileVersions'][0]['snapshotId']
+            print("oldest: %s snapshot: %s"%(oldest_v, oldest_s))
+            newest_v, newest_s = file['fileVersions'][-1]['lastModified'], file['fileVersions'][-1]['snapshotId']
+            print("newest: %s snapshot: %s \n"%(newest_v, newest_s))
+            if 'expand' in kw:
+                print("Expanded Version List:")
+                for fv in file['fileVersions']:
+                    print("date: %s snapshot: %s"%(fv['lastModified'], fv['snapshotId']))
+                print("\n")
+        
+
+def fileset(name, host, search, **kw):
+    """
+        searches name / host for FS ID and lists files matching search-path and associated snapshots.
+    """
+    header = "--header 'Accept: application/json'"
+    url = "'https://%s/api/v1/fileset?name=%s&host_name=%s'"%(cdm,name,host)
+    curl = "curl -s -X GET %s %s %s --insecure"%(auth, header, url)
+    print (curl)
+    response = get_curl_response(curl)
+    fileSetId = None
+    if response['total'] > 1:
+        print("Found more than 1 fileSet with name: %s & host: %s"%(name, host))
+        for fId in response['data']:
+            print("name: %s id: %s"%(fId['name'], fId['id']))
+            if name == fId['templateName']:
+                fileSetId = fId
+    else:
+        if response['total'] > 0:
+            fileSetId = response['data'][0]
+        else:
+            print("No entries found for provided fileset/host combination")
+            return
+    
+    print("Using %s -- %s"%(fileSetId['name'], fileSetId['id']))
+    url = "'https://%s/api/v1/fileset/%s/search?path=%s'"%(cdm, quote(fileSetId['id'], safe=''), quote(search, safe=''))
+    curl = "curl -s -X GET %s %s %s --insecure"%(auth, header, url)
+    print(curl)
+    response = get_curl_response(curl)
+    if response['total'] > 0:
+        list_files(response['data'], kw)
+    else:
+        print("No files found matching provided using search string: %s in fileset: %s for host: %s"%(search, name, host))
+
+def vm(name, search, **kw):
+    """
+        get VM ID:
+    """
+    header = "--header 'Accept: application/json'"
+    url = "'https://%s/api/v1/vmware/vm?name=%s'"%(cdm,name)
+    curl = "curl -s -X GET %s %s %s --insecure"%(auth,header, url)
+    print(curl)
+    response = get_curl_response(curl)
+    if not 'total' in response:
+        try:
+            print(response['message'])
+        except:
+            print(response)
+        return
+    if int(response['total']) > 1:
+        print ("Multiple ID's found with the provided vmware name: %s"%(name))
+        for resp in response['data']:
+            print(response['data'][resp]['id'])
+            return
+    else:
+        if response['total'] == 0:
+            print("No vm id found with matching name: %s"%(name))
+            return
+        print("Found vm id matching name: %s"%(name))
+        vmid=response['data'][0]['id']
+        url = "'https://%s/api/v1/vmware/vm/%s/search?path=%s'"%(cdm,vmid, search)
+        curl = "curl -s -X GET %s %s %s --insecure"%(auth, header, url)
+        print(curl)
+        response = get_curl_response(curl)
+        if response['total'] > 0:
+            list_files(response['data'], kw)
+        else:
+            print("Not files found matching search string: %s in vm: %s"%(search, name))
+def main():
+    if auth is None:
+        print("""
+        Missing auth.cfg file
+        Run:
+            echo -n "<path-to-auth-file>" > auth.cfg
+        Also required:
+            echo -n "admin:password" > path-to-auth-file # Keep this file in safe place
+        
+        Example:
+            echo -n "~/cdm_auth" > auth.cfg
+            echo -n "admin:abcd1234" > ~/cdm_auth
+        """)
+        return
+    import sys
+    args = sys.argv
+    global cdm
+    try:
+        cdm, objtype, name = args[1], args[2], args[3]
+    except:
+        if len(args) < 4:
+            if len(args) < 3:
+                if len(args) < 2:
+                    print("Missing CDM IP")
+                print("Missing objtype vm|fileset")
+            print("Missing name")
+        usage()
+        return
+    if objtype == 'fileset':
+        try:
+            host, searchString = args[4], args[5]
+        except:
+            if len(args) < 6:
+                if len(args) < 5:
+                    print("Missing field: Host")
+                print("Missing search string")
+            usage()
+            return
+        if len(args) > 6:
+            kwArgs = args[6]
+            if kwArgs == '--expand':
+                fileset(name,host, searchString, expand=True)
+            elif kwArgs == '--files':
+                fileset(name,host, searchString, files=True)
+            else:
+                print("Unrecognized argument: %s"%(kwArgs))
+                usage()
+        else:
+            fileset(name,host, searchString)
+    elif objtype =='vm':
+        if len(args) > 4: 
+            searchString = args[4]
+        else:
+            print("Missing filed: searchString")
+            usage()
+            return
+        if len(args) > 5:
+            kwArgs = args[5]
+            if kwArgs == '--expand':
+                vm(name, searchString, expand=True)
+            elif kwArgs == '--files':
+                vm(name, searchString, files=True)
+            else:
+                print("Unrecognized argument: %s"%(kwArgs))
+                usage()
+        else:
+            vm(name, searchString)
+    else:
+        usage()
+def usage():
+    print("""
+        Usage: search.py <cdm_ip> <vm> <name> <searchString> [--files|--expand]
+
+        Usage: search.py <cdm_ip> <fileset> <name> <host> <searchString> [--files|--expand]
+
+        --files: view file names only 
+    """)
+
+if __name__ == '__main__':
+    main()
